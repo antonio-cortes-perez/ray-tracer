0285ae3a7a335a2453ded8a5c53b673859c70b68
Antonio <antonio.cortes.perez@gmail.com>
2022-12-24 11:39:17 -0800

    Matrix transpose, determinant, inverse.

--- include/Matrix.h
+++ include/Matrix.h
@@ -11,9 +11,12 @@ using M2x2 = std::array<std::array<float, 2>, 2>;
 using M3x3 = std::array<std::array<float, 3>, 3>;
 using M4x4 = std::array<std::array<float, 4>, 4>;
 
+static constexpr M4x4 I4x4 = {
+    {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}};
+
 template <unsigned long M>
-bool eq(const std::array<std::array<float, M>, M> &m0,
-        const std::array<std::array<float, M>, M> &m1) {
+inline bool eq(const std::array<std::array<float, M>, M> &m0,
+               const std::array<std::array<float, M>, M> &m1) {
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < M; ++j) {
       if (!eqf(m0[i][j], m1[i][j]))
@@ -41,6 +44,78 @@ inline Tuple mult(const M4x4 &m, const Tuple &t) {
   r.z = m[2][0] * t.x + m[2][1] * t.y + m[2][2] * t.z + m[2][3] * t.w;
   r.w = m[3][0] * t.x + m[3][1] * t.y + m[3][2] * t.z + m[3][3] * t.w;
   return r;
+}
+
+inline M4x4 transpose(const M4x4 &m) {
+  M4x4 t;
+  for (int row = 0; row < 4; ++row) {
+    for (int col = 0; col < 4; ++col) {
+      t[col][row] = m[row][col];
+    }
+  }
+  return t;
+}
+
+inline float determinant(const M2x2 &m) {
+  return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+}
+
+template <unsigned long M>
+inline std::array<std::array<float, M - 1>, M - 1>
+submatrix(const std::array<std::array<float, M>, M> &m, int skipRow,
+          int skipCol) {
+  std::array<std::array<float, M - 1>, M - 1> s;
+  for (int row = 0, subRow = 0; row < M; ++row) {
+    if (row == skipRow)
+      continue;
+    for (int col = 0, subCol = 0; col < M; ++col) {
+      if (col == skipCol)
+        continue;
+      s[subRow][subCol] = m[row][col];
+      ++subCol;
+    }
+    ++subRow;
+  }
+  return s;
+}
+
+template <unsigned long M>
+inline float determinant(const std::array<std::array<float, M>, M> &m) {
+  float det = 0;
+  for (int col = 0; col < M; ++col) {
+    float minor = determinant(submatrix(m, 0, col));
+    float cofactor = (col % 2 == 0) ? minor : -minor;
+    det += m[0][col] * cofactor;
+  }
+  return det;
+}
+
+template <unsigned long M>
+inline float minor(const std::array<std::array<float, M>, M> &m, int row,
+                   int col) {
+  return determinant(submatrix(m, row, col));
+}
+
+template <unsigned long M>
+inline float cofactor(const std::array<std::array<float, M>, M> &m, int row,
+                      int col) {
+  return ((row + col) % 2 == 0) ? minor(m, row, col) : -minor(m, row, col);
+}
+
+inline bool isInvertible(const M4x4 &m) { return !eqf(determinant(m), 0); }
+
+inline M4x4 inverse(const M4x4 &m) {
+  float det = determinant(m);
+  assert(!eqf(det, 0));
+
+  M4x4 result;
+  for (int row = 0; row < 4; ++row) {
+    for (int col = 0; col < 4; ++col) {
+      float cof = cofactor(m, row, col);
+      result[col][row] = cof / det;
+    }
+  }
+  return result;
 }
 
 } // namespace ray_tracer
--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -31,3 +31,80 @@ TEST(MatrixTest, MatrixXTuple) {
   Tuple t(1, 2, 3, 1);
   EXPECT_EQ(mult(m, t), Tuple(18, 24, 33, 1));
 }
+
+TEST(MatrixTest, IdentityXMatrix) {
+  M4x4 m = {{{0, 1, 2, 4}, {1, 2, 4, 8}, {2, 4, 8, 16}, {4, 8, 16, 32}}};
+  EXPECT_TRUE(eq(mult(m, I4x4), m));
+}
+
+TEST(MatrixTest, IdentityXTuple) {
+  Tuple t(1, 2, 3, 4);
+  EXPECT_EQ(mult(I4x4, t), t);
+}
+
+TEST(MatrixTest, Transpose) {
+  M4x4 m = {{{0, 9, 3, 0}, {9, 8, 0, 8}, {1, 8, 5, 3}, {0, 0, 5, 8}}};
+  M4x4 t = {{{0, 9, 1, 0}, {9, 8, 8, 0}, {3, 0, 5, 5}, {0, 8, 3, 8}}};
+  EXPECT_TRUE(eq(transpose(m), t));
+}
+
+TEST(MatrixTest, TransposeIdentity) { EXPECT_TRUE(eq(transpose(I4x4), I4x4)); }
+
+TEST(MatrixTest, Determinant2x2) {
+  M2x2 m = {{{1, 5}, {-3, 2}}};
+  EXPECT_FLOAT_EQ(determinant(m), 17);
+}
+
+TEST(MatrixTest, SubmatrixM4x4) {
+  M4x4 m = {{{-6, 1, 1, 6}, {-8, 5, 8, 6}, {-1, 0, 8, 2}, {-7, 1, -1, 1}}};
+  M3x3 s = {{{-6, 1, 6}, {-8, 8, 6}, {-7, -1, 1}}};
+  EXPECT_TRUE(eq(submatrix(m, 2, 1), s));
+}
+
+TEST(MatrixTest, SubmatrixM3x3) {
+  M3x3 m = {{{1, 5, 0}, {-3, 2, 7}, {0, 6, -3}}};
+  M2x2 s = {{{-3, 2}, {0, 6}}};
+  EXPECT_TRUE(eq(submatrix(m, 0, 2), s));
+}
+
+TEST(MatrixTest, Minor) {
+  M3x3 m = {{{3, 5, 0}, {2, -1, -7}, {6, -1, 5}}};
+  EXPECT_FLOAT_EQ(minor(m, 1, 0), 25);
+}
+
+TEST(MatrixTest, Cofactor) {
+  M3x3 m = {{{3, 5, 0}, {2, -1, -7}, {6, -1, 5}}};
+  EXPECT_FLOAT_EQ(cofactor(m, 0, 0), -12);
+  EXPECT_FLOAT_EQ(cofactor(m, 1, 0), -25);
+}
+
+TEST(MatrixTest, Determinant3x3) {
+  M3x3 m = {{{1, 2, 6}, {-5, 8, -4}, {2, 6, 4}}};
+  EXPECT_FLOAT_EQ(determinant(m), -196);
+}
+
+TEST(MatrixTest, Determinant4x4) {
+  M4x4 m = {{{-2, -8, 3, 5}, {-3, 1, 7, 3}, {1, 2, -9, 6}, {-6, 7, 7, -9}}};
+  EXPECT_FLOAT_EQ(determinant(m), -4071);
+}
+
+TEST(MatrixTest, Invertible) {
+  M4x4 m = {{{6, 4, 4, 4}, {5, 5, 7, 6}, {4, -9, 3, -7}, {9, 1, 7, -6}}};
+  EXPECT_FLOAT_EQ(determinant(m), -2120);
+  EXPECT_TRUE(isInvertible(m));
+}
+
+TEST(MatrixTest, NonInvertible) {
+  M4x4 m = {{{-4, 2, -2, -3}, {9, 6, 2, 6}, {0, -5, 1 - 5}, {0, 0, 0, 0}}};
+  EXPECT_FLOAT_EQ(determinant(m), 0);
+  EXPECT_FALSE(isInvertible(m));
+}
+
+TEST(MatrixTest, Inverse0) {
+  M4x4 m = {{{-5, 2, 6, -8}, {1, -5, 1, 8}, {7, 7, -6, -7}, {1, -3, 7, 4}}};
+  M4x4 inv = {{{0.21805, 0.45113, 0.24060, -0.04511},
+               {-0.80827, -1.45677, -0.44361, 0.52068},
+               {-0.07895, -0.22368, -0.05263, 0.19737},
+               {-0.52256, -0.81391, -0.30075, 0.30639}}};
+  EXPECT_TRUE(eq(inverse(m), inv));
+}
